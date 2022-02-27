d3024c12ade8c744ab1e58eb26a4f80bff7b1680
Sandra S S <41688158+SandyVR46@users.noreply.github.com>
2022-02-27 12:06:06 +0530

    stack using linked list - push, pop, search

--- Advanced Data Structures/STACKLL.C
+++ Advanced Data Structures/STACKLL.C
@@ -1,119 +1,149 @@
-#include <stdio.h>  
-#include <stdlib.h>  
-void push();  
-void pop();  
-void display();  
-struct node   
-{  
-int val;  
-struct node *next;  
-};  
-struct node *head;  
-  
+#include <stdio.h>
+#include <stdlib.h>
+void push();
+void pop();
+void display();
+void search();
+struct node
+{
+int data;
+struct node *link;
+};
+struct node *top;
 void main ()
 {
-    int choice=0;
-    clrscr();
-    printf("               **********STACK USING SINGLY LINKLIST**********               ");
-    while(choice != 4)
-    {
-	printf("\n\nChose one from the below options...\n");
-	printf("\n1.Push\n2.Pop\n3.Show\n4.Exit");
-	printf("\n Enter your choice \n");
-	scanf("%d",&choice);
-	switch(choice)
-	{
-	    case 1:
-	    {
-		push();
-		break;
-	    }
-	    case 2:
-	    {
-		pop();
-		break;
-	    }
-	    case 3:
-	    {
-		display();
-		break;
-	    }
-	    case 4:
-	    {
-		printf("Exiting....");
-		break;
-	    }
-	    default:
-	    {
-		printf("Please Enter valid choice ");
-	    }
-    };
+int ch,n;
+clrscr();
+printf(" **********STACK USING SINGLY LINKLIST********** ");
+while(ch != 5)
+{
+printf("\n\nChose one from the below options...\n");
+printf("\n1.Push\n2.Pop\n3.Display\n4.Search\n5.Exit");
+printf("\n Enter your choice \n");
+scanf("%d",&ch);
+switch(ch)
+{
+case 1:
+{
+push();
+break;
+}
+case 2:
+{
+pop();
+break;
+}
+case 3:
+{
+display();
+break;
+}
+case 4:
+{
+search();
+break;
+}
+case 5:
+{
+exit(0);
+}
+default:
+{
+printf("Please Enter valid choice ");
+}
+};
 }
 }
 void push ()
 {
-    int val;
-    struct node *ptr = (struct node*)malloc(sizeof(struct node));
-    if(ptr == NULL)
-    {
-	printf("not able to push the element");
-    }
-    else
-    {
-	printf("\nEnter the value: ");
-	scanf("%d",&val);
-	if(head==NULL)
-	{
-	    ptr->val = val;
-	    ptr -> next = NULL;
-	    head=ptr;
-	}
-	else
-	{
-	    ptr->val = val;
-	    ptr->next = head;
-	    head=ptr;
-
-	}
-	printf("\nItem pushed");
-
-    }
+int ele;
+struct node *newNode;
+newNode = (struct node*)malloc(sizeof(struct node));
+if(newNode == NULL)
+{
+printf("not able to push the element");
 }
-
+else
+{
+printf("\nEnter the element: ");
+scanf("%d",&ele);
+newNode -> data = ele;
+if(top==NULL)
+{
+newNode -> link =NULL;
+}
+else
+{
+newNode -> link = top;
+}
+top = newNode;
+printf("\nItem pushed");
+}
+}
 void pop()
 {
-    int item;
-    struct node *ptr;
-    if (head == NULL)
-    {
-	printf("\nUnderflow");
-    }
-    else
-    {
-	item = head->val;
-	ptr = head;
-	head = head->next;
-	free(ptr);
-	printf("\nItem popped");
-
-    }
+int item;
+struct node *temp;
+if (top == NULL)
+{
+printf("\nUnderflow");
 }
+else
+{
+item = top->data;
+temp = top;
+top = top -> link;
+free(temp);
+printf("\nItem popped -> %d",item);
+}
+}
+
 void display()
 {
-    int i;
-    struct node *ptr;
-    ptr=head;
-    if(ptr == NULL)
-    {
-	printf("\nStack is empty\n");
-    }
-    else
-    {
-	printf("\nPrinting Stack elements \n");
-	while(ptr!=NULL)
-	{
-	    printf("\n%d\n",ptr->val);
-	    ptr = ptr->next;
-	}
-    }
+int i;
+struct node *ptr;
+if(top == NULL)
+{
+printf("\nStack is empty\n");
+}
+else
+{
+ptr=top;
+printf("\nPrinting Stack elements \n");
+while(ptr!=NULL)
+{
+printf("\n%d\n",ptr->data);
+ptr = ptr->link;
+}
+}
+}
+void search()
+{
+int i=1, flag=0, item;
+if(top == NULL)
+{
+printf("\n List is empty");
+return;
+}
+while(top != NULL)
+{
+if(top->data==item)
+   {
+      flag=1;
+      break;
+   }
+else
+{
+   top=top->link;
+   i++;
+}
+if(flag==1)
+{
+   printf("%d found", item);
+}
+else
+{
+printf("\n %d Element not found", item);
+}
+}
 }
